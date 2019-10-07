fbab6c69b253cd156fe5130384cc1983b5828b2c
nattthebear <goyuken@gmail.com>
2019-10-06 20:25:09 -0400

    Greatly speed up the optimizer by replacing the brute force method with something that swaps out one piece of gear at a time, continuing until it can make no improvements (local maximum).
    Given the simplicity of FFXII's game mechanics, I think this will always find the correct solution.

--- src/dps/Optimize.ts
+++ src/dps/Optimize.ts
@@ -87,26 +87,84 @@ export function optimize(startingProfile: Profile, e: Environment, weapon: Equip
 	}
 
 	let topDps = 0;
-	// let topProfile = initialProfile;
 	let topDoll = initialDoll;
-	for (const armor of armors) {
-		for (const helm of helms) {
-			for (const accessory of accessories) {
+	{
+		let armor: Equipment | undefined = undefined;
+		let helm: Equipment | undefined = undefined;
+		let accessory: Equipment | undefined = undefined;
+		let iter = 0;
+		for (; iter < 10; iter++) {
+			let changed = false;
+			for (const nextArmor of armors) {
 				const doll: PaperDoll = {
 					weapon,
 					ammo,
-					armor,
+					armor: nextArmor,
 					helm,
 					accessory
 				};
 				const p = createProfile(startingProfile, doll);
 				const dps = calculate(p, e);
 				if (dps > topDps) {
+					// console.log(`${armor && armor.name} => ${nextArmor && nextArmor.name}`);
+					armor = nextArmor;
 					topDps = dps;
-					// topProfile = p;
 					topDoll = doll;
+					changed = true;
 				}
 			}
+			for (const nextHelm of helms) {
+				const doll: PaperDoll = {
+					weapon,
+					ammo,
+					armor,
+					helm: nextHelm,
+					accessory
+				};
+				const p = createProfile(startingProfile, doll);
+				const dps = calculate(p, e);
+				if (dps > topDps) {
+					// console.log(`${helm && helm.name} => ${nextHelm && nextHelm.name}`);
+					helm = nextHelm;
+					topDps = dps;
+					topDoll = doll;
+					changed = true;
+				}
+			}
+			for (const nextAccessory of accessories) {
+				const doll: PaperDoll = {
+					weapon,
+					ammo,
+					armor,
+					helm,
+					accessory: nextAccessory
+				};
+				const p = createProfile(startingProfile, doll);
+				const dps = calculate(p, e);
+				if (dps > topDps) {
+					// console.log(`${accessory && accessory.name} => ${nextAccessory && nextAccessory.name}`);
+					accessory = nextAccessory;
+					topDps = dps;
+					topDoll = doll;
+					changed = true;
+				}
+			}
+			if (!changed) {
+				break;
+			}
+		}
+		if (iter > 2) {
+			// weapons that do no damage under any cirumstances (att * 1.125 < def) take 1 iteration;
+			// because everything does 0 damage, the optimizer can't observe any changes and just gives up and goes naked
+
+			// most normal circumstances take 2 iterations, as each choice is stable against the other choices,
+			// so the second iteration is only needed to confirm everything
+
+			// some unusal circumstances take 3 iterations.  for instance, unarmed with target def >= ~15 and no brawler
+			// takes an entire iteration to find the jade collar, and then another iteration to choose other pieces
+
+			// anything more than 3 is unusual and likely indicates some sort of fubar bug
+			console.warn(`${iter + 1} iterations to optimize ${weapon.name}, maybe wrong?`);
 		}
 	}
 	return {
