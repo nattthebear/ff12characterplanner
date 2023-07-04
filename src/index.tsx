47e9ef0c5528925c165ed4b19638fec3e1630bbf
nattthebear <goyuken@gmail.com>
2023-07-04 13:56:31 -0400

    fix:  tooltip needs to be at the end of the body to show up on top of other elements

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import { h, createRoot } from "vdomk";
 import App from "./component/App";
 import "./MouseOver";
 
-createRoot(document.body, <App />);
+createRoot(
+	document.body,
+	<App />,
+	document.body.lastChild // tooltip must come after the app root
+);
