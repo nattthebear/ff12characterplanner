ea19b9f501c55a88ac9a92582e97fd96566131ba
nattthebear <goyuken@gmail.com>
2023-07-09 20:12:12 -0400

    Polyfill some unsupported features in Safari

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,27 @@
+if (!Array.prototype.at) {
+	Object.defineProperty(Array.prototype, "at", {
+		configurable: true,
+		enumerable: false,
+		value: function at(index: number) {
+			const { length } = this;
+			if (index < 0) {
+				index += length;
+			}
+			if (index >= 0 && index < length) {
+				return this[index];
+			}
+		},
+	});
+}
+if (!window.requestIdleCallback) {
+	window.requestIdleCallback = function requestIdleCallback(callback) {
+		return setTimeout(callback, 0);
+	};
+	window.cancelIdleCallback = function cancelIdleCallback(handle) {
+		clearTimeout(handle);
+	};
+}
+
 import { h, createRoot } from "vdomk";
 import App from "./component/App";
 import "./MouseOver";
