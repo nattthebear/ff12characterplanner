0720e5aa4088c0f5d05586ec4e2f94296a61ea8c
nattthebear <goyuken@gmail.com>
2024-05-24 11:52:02 -0400

    minor cleanup

--- src/model/Heap.ts
+++ src/model/Heap.ts
@@ -8,43 +8,45 @@ export class Heap<T> {
 	}
 
 	insert(element: T) {
-		let index = this.nodes.length;
-		this.nodes.push(element);
+		const { nodes, compare } = this;
+		let index = nodes.length;
+		nodes.push(element);
 		while (index > 0) {
 			const parent = (index - 1) >> 1;
-			if (!this.compare(this.nodes[index], this.nodes[parent])) {
+			if (!compare(nodes[index], nodes[parent])) {
 				return;
 			}
-			const tmp = this.nodes[index];
-			this.nodes[index] = this.nodes[parent];
-			this.nodes[parent] = tmp;
+			const tmp = nodes[index];
+			nodes[index] = nodes[parent];
+			nodes[parent] = tmp;
 			index = parent;
 		}
 	}
 
 	remove() {
-		if (this.nodes.length < 2) {
-			return this.nodes.pop();
+		const { nodes, compare } = this;
+		if (nodes.length < 2) {
+			return nodes.pop();
 		}
-		const oldRoot = this.nodes[0];
-		const newRoot = this.nodes.pop()!;
-		this.nodes[0] = newRoot;
+		const oldRoot = nodes[0];
+		const newRoot = nodes.pop()!;
+		nodes[0] = newRoot;
 		let index = 0;
 		while (true) {
 			const left = 2 * index + 1;
 			const right = left + 1;
 			let nextIndex = index;
-			if (left < this.nodes.length && this.compare(this.nodes[left], newRoot)) {
+			if (left < nodes.length && compare(nodes[left], newRoot)) {
 				nextIndex = left;
 			}
-			if (right < this.nodes.length && this.compare(this.nodes[right], this.nodes[nextIndex])) {
+			if (right < nodes.length && compare(nodes[right], nodes[nextIndex])) {
 				nextIndex = right;
 			}
 			if (nextIndex === index) {
 				return oldRoot;
 			}
-			this.nodes[index] = this.nodes[nextIndex];
-			this.nodes[nextIndex] = newRoot;
+			nodes[index] = nodes[nextIndex];
+			nodes[nextIndex] = newRoot;
 			index = nextIndex;
 		}
 	}
