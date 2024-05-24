0e1d4da414434d27979d9992e64890fcbb7f452d
nattthebear <goyuken@gmail.com>
2024-05-24 11:58:21 -0400

    Slightly decrease Fran's strength
    This is consistent with https://finalfantasy.fandom.com/wiki/Final_Fantasy_XII_stats, which doesn't have values for every level and so can't detect these small differences.  It's also supported by https://docs.google.com/spreadsheets/d/1tVJfvldSITYxijEPwZ7qL01MGwdQKAkYXxp3ZGAv53I which has values for every level.
    
    With this change, the values in BaseCharacterStats.ts now exactly match both of those sources everywhere except a few single level discrepancies that must be errors in the source sheets.

--- src/dps/BaseCharacterStats.ts
+++ src/dps/BaseCharacterStats.ts
@@ -1,18 +1,15 @@
-// TODO: Verify these.  They were made by using the formula documented for classic with the stats on the wiki for izjs/tza
-// Everything matches except Balthier's mag value?
-
 const baseValues = {
-	str: [23, 24, 23, 25, 21, 20, ],
+	str: [23, 24, 22, 25, 21, 20, ],
 	mag: [22, 18, 20, 20, 24, 21, ],
 	vit: [24, 24, 26, 20, 23, 23, ],
-	spd: [24, 24, 23, 23, 23, 24, ]
+	spd: [24, 24, 23, 23, 23, 24, ],
 };
 
 const modifiers = {
-	str: [72, 67, 63, 69, 68, 65, ],
+	str: [72, 67, 64, 69, 68, 65, ],
 	mag: [67, 59, 63, 56, 70, 72, ],
 	vit: [48, 45, 47, 36, 44, 48, ],
-	spd: [18, 19, 18, 16, 17, 16, ]
+	spd: [18, 19, 18, 16, 17, 16, ],
 };
 
 /** Get the base stats for a character at a particular level */
