92f070991f5f3619d4b2107715ee29c98fabdd3e
nattthebear <goyuken@gmail.com>
2026-02-06 16:41:45 -0500

    implement with native anchor

--- src/component/MouseOver.tsx
+++ src/component/MouseOver.tsx
@@ -1,37 +1,28 @@
-import { computePosition, shift, flip } from "@floating-ui/dom";
-import { TPC, effect, h, scheduleUpdate } from "vdomk";
+import { TPC, h, scheduleUpdate } from "vdomk";
 import "./MouseOver.css";
 
 const Tooltip: TPC<{}> = (_, instance) => {
-	let floatingElement: HTMLDivElement | null | undefined;
-	const ref = (value: HTMLDivElement | null) => floatingElement = value;
-
 	let labelText: string | null | undefined;
-	let referenceElement: Element | null | undefined;
+	let referenceElement: HTMLElement | null | undefined;
 
 	function handleMouseOver(event: MouseEvent) {
-		referenceElement = event.target as Element | null;
+		const previousReferenceElement = referenceElement;
+		referenceElement = event.target as HTMLElement | null;
 		while (referenceElement && (labelText = referenceElement.getAttribute("aria-label")) == null) {
 			referenceElement = referenceElement.parentElement;
 		}
-		scheduleUpdate(instance);		
+		if (previousReferenceElement !== referenceElement) {
+			previousReferenceElement?.style.removeProperty("anchor-name");
+			referenceElement?.style.setProperty("anchor-name", "--tooltip");
+			scheduleUpdate(instance);
+		}
 	}
 
 	document.addEventListener("mouseover", handleMouseOver, { passive: true });
 	document.documentElement.addEventListener("mouseleave", handleMouseOver, { passive: true });
 
-	async function updateStyles() {
-		if (referenceElement && floatingElement) {
-			const styles = await computePosition(referenceElement, floatingElement, { middleware: [flip(), shift()], strategy: "fixed" });
-			floatingElement.style.transform = `translate(${Math.round(styles.x)}px,${Math.round(styles.y)}px)`;
-		}
-	}
-
-	document.addEventListener("scroll", updateStyles, { passive: true, capture: true });
-
 	return () => {
-		effect(instance, updateStyles);
-		return <div ref={ref} class={labelText ? "tooltip" : "tooltip hidden"}>{labelText}</div>;
+		return <div class={labelText ? "tooltip" : "tooltip hidden"}>{labelText}</div>;
 	};
 };
 
